0f2e6f778dd4488b08e8b7ffe21d2c8555a0d014
Afroj Ahmad <itsafroj@gmail.com>
2025-01-02 15:01:28 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+function Header() {
+  return (
+    <header>
+      <h1><MenuBookIcon />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
